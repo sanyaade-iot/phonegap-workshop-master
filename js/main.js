8dbc019150ff13d9029cb778aa2c6d4428e2b918
William Pietri <william@scissor.com>
2014-04-05 17:11:56 -0700

    using phonegap for an alert.

--- js/main.js
+++ js/main.js
@@ -14,10 +14,20 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new LocalStorageStore();
+        this.store = new LocalStorageStore(function() { app.showAlert('Store Initialized', 'Info');});
+
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
     }
 
 };
 
 app.initialize();
+
